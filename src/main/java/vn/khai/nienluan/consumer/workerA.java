b9cd596fa543c1c87e63f8a0511fc2850fe2ad8e
toanhkhai <toanhkhai12345@gmail.com>
2026-04-11 21:49:30 +0700

    change code workerA and add comment

--- src/main/java/vn/khai/nienluan/consumer/workerA.java
+++ src/main/java/vn/khai/nienluan/consumer/workerA.java
@@ -2,6 +2,7 @@ package vn.khai.nienluan.consumer;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -12,7 +13,7 @@ import vn.khai.nienluan.model.SearchRequest;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Slf4j
 @Component
 public class workerA {
 
@@ -28,40 +29,47 @@ public class workerA {
         String apiUrl = "https://openlibrary.org/search.json?q=" + searchKeyword + "&limit=10";
 
         try {
+            //Gọi API lấy JSON về từ OpenLibrary
             String jsonResponse = restTemplate.getForObject(apiUrl, String.class);
 
+            //Chuyển đổi Json dạng String sang JsonNode
             ObjectMapper mapper = new ObjectMapper();
             JsonNode root = mapper.readTree(jsonResponse);
+
+            //Lấy ra docs (danh sách những cuốn sách)
             JsonNode docs = root.path("docs");
 
             List<BookResponse> results = new ArrayList<>();
 
             if (docs.isArray() && !docs.isEmpty()) {
-
+                //lấy tối đa 7 cuốn sách
                 int maxResults = Math.min(docs.size(), 7);
-
                 for (int i = 0; i < maxResults; i++) {
                     JsonNode book = docs.get(i);
-
+                    //Lấy ra title và tác giả.
                     String title = book.path("title").asText();
                     String author = "Chưa rõ tác giả";
                     if (book.has("author_name") && book.path("author_name").isArray()) {
                         author = book.path("author_name").get(0).asText();
                     }
-
+                    //Lấy ra key của cuốn sách
                     String workKey = book.path("key").asText();
+
+                    //url https://openlibrary.org + key cuốn sách -> link cuốn sách.
                     String directLink = "https://openlibrary.org" + workKey;
 
                     String finalTitle = title + " (Tác giả: " + author + ")";
+
+                    //Dữ liệu phản hồi bao gồm : Tên tác giả + link url sách + nguồn thư viện
                     BookResponse result = new BookResponse(finalTitle, directLink, "Open Library API");
                     results.add(result);
                 }
 
-                // Gửi toàn bộ danh sách kết quả
+                // Gửi toàn bộ danh sách kết quả cho message -> PHẢI CÓ SESSION ID để gửi đúng client cần nhận
                 String destination = "/topic/results/" + request.getSessionId();
                 messagingTemplate.convertAndSend(destination, results);
             } else {
-                System.out.println("Worker A không tìm thấy kết quả cho keyword: " + request.getKeyword());
+                System.out.println("Không tìm thấy kết quả cho keyword: " + request.getKeyword());
             }
 
         } catch (Exception e) {
