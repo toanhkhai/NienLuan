a590706afe60a45bb2aa796e02c9aa5db7830016
toanhkhai <toanhkhai12345@gmail.com>
2026-04-19 12:57:41 +0700

    change code config RabbitMQ

--- src/main/java/vn/khai/nienluan/config/RabbitMQConfig.java
+++ src/main/java/vn/khai/nienluan/config/RabbitMQConfig.java
@@ -4,54 +4,70 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    //Cau hinh cac hang doi - tao ra 3 hang doi
     @Bean
-    public Queue queueA(){
-        return new Queue("queueA",true);
+    public Queue queueA() {
+        return new Queue("queueA", true);
     }
 
     @Bean
-    public Queue queueB(){
-        return new Queue("queueB",true);
+    public Queue queueB() {
+        return new Queue("queueB", true);
     }
 
     @Bean
-    public Queue queueC(){
-        return new Queue("queueC",true);
+    public Queue queueC() {
+        return new Queue("queueC", true);
     }
 
-    //Cau hinh Exchange
+    // Cấu hình Exchange
     @Bean
-    public FanoutExchange fanoutExchange(){
+    public FanoutExchange fanoutExchange() {
+
         return new FanoutExchange("fanout-exchange");
     }
 
-    //Cau hinh Binding
-
+    // Cấu hình Binding
     @Bean
-    public Binding queueA_Binding(){
+    public Binding queueA_Binding() {
         return BindingBuilder
                 .bind(queueA())
                 .to(fanoutExchange());
     }
 
     @Bean
-    public Binding queueB_Binding(){
+    public Binding queueB_Binding() {
         return BindingBuilder
                 .bind(queueB())
                 .to(fanoutExchange());
     }
 
     @Bean
-    public Binding queueC_Binding(){
+    public Binding queueC_Binding() {
         return BindingBuilder
                 .bind(queueC())
                 .to(fanoutExchange());
+    }
+
+    //Message converter cho JSON
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // Cấu hình RabbitTemplate để dùng JSON converter
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
     }
 }
